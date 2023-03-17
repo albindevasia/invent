5970a7c4254ac53384ae29c4ed3881aad31389d0
albindevasia <114718268+albindevasia@users.noreply.github.com>
2023-03-17 09:56:16 +0530

    upf

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -3,29 +3,38 @@ import {
   EventEmitter,
   Input,
   OnChanges,
+  OnInit,
   Output,
   SimpleChanges,
 } from '@angular/core';
+import {ChangeDetectorRef } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent implements OnChanges {
+export class PaginationComponent implements OnChanges  {
+
+
+  constructor(private cdr: ChangeDetectorRef) {}
+  
+  
+@Input() products$!:Observable<any>
   @Input() currentPage: number = 1;
-  @Input() totalItems!: number;
+  // @Input() totalItems!: number;
   @Input() pageSize: number = 5;
   @Output() pageChanged = new EventEmitter();
- 
 
- 
+  toPage!:number
+
   get totalPages(): number {
-    return Math.ceil(this.totalItems/ this.pageSize);
+    return Math.ceil(this.toPage/ this.pageSize);
   }
 
 
-  get pages(): number[] {
+    get pages(): number[] {
     const pages: number[] = [];
 
     for (let i = 1; i <= this.totalPages; i++) {
@@ -35,16 +44,44 @@ export class PaginationComponent implements OnChanges {
     return pages;
   }
   
-
+ 
+  // ngOnInit(){
+  //   this.pages()
+  // }
+   pagetotal!:number
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['totalItems']) {
-      console.log(changes);
+    // console.log("jhjht")
+    // // console.log(this.currentPage);
+    // console.log(changes['totalItems']);
+
+    // if (changes['totalItems']) {
+    //   console.log(changes);
+
+    //    this.currentPage = 1;
+    //   console.log(this.currentPage);
+
+    this.products$.subscribe((product)=>{
+      this.toPage=product.length
+      
+    })
+
        this.currentPage = 1;
+
+      // this.pageChanged.emit({
+      //   currentPage: this.currentPage,
+      // });
+  
+//
+    
+
+// this.cdr.detectChanges();
+
+
       // this.pageChanged.emit({
       //   currentPage: this.currentPage,
       // });
     }
-  }
+  
   nextPage(currentPage: number): void {
     this.pageChanged.emit(currentPage);
     if (this.currentPage < this.totalPages) {
