826d3e24280c6f92c13bf8e0ea361193e7b0bf58
albindevasia <114718268+albindevasia@users.noreply.github.com>
2023-04-06 11:34:54 +0530

    v

--- src/app/quicksale/quicksale.component.ts
+++ src/app/quicksale/quicksale.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-quicksale',
@@ -12,15 +16,20 @@ export class QuicksaleComponent {
   quickSale$!:Observable<any>;
 
   constructor(
+    private readonly toastr: ToastrService,
+    private readonly http: HttpClient,
     private readonly apiService:ApiService,
     private readonly router:Router
   ){
 
   }
-
-
+  public productApi: string = 'https://api-sales-app.josetovar.dev/products';
+  public quickApi: string = 'https://api-sales-app.josetovar.dev/quick-sales';
+  public filteredProducts: any[] = [];
+  public products$!: Observable<any>;
   ngOnInit(){
    this.getQuickSale();
+   this.products$=this.http.get(this.productApi);
     }
 
     getQuickSale(){
@@ -34,4 +43,78 @@ export class QuicksaleComponent {
       this.router.navigate(['dashboard/sales/newsale'],{queryParams:{quick_sale:id}})
     
     }
+
+    posting:boolean=false;
+    
+
+  public postNew(){
+    this.posting=true;
+  }  
+  public closeNew(){
+    this.posting=false;
+
+  }
+  
+  public quickForm:FormGroup = new FormGroup ({
+    name: new FormControl('',Validators.required),
+    products:new FormArray([])
+  })
+
+  get selectedQuick():FormArray{
+    return this.quickForm.get('products') as FormArray
+
+  }
+
+  public addToquick(){
+    const newProduct:FormGroup= new FormGroup({
+      id:new FormControl(null,Validators.required),
+    })
+    this.selectedQuick.push(newProduct);
+  }
+  public removeProduct(productIndex: any): void {
+        this.selectedQuick.removeAt(productIndex);
+      }
+
+  searchText:string='';
+  searchProduct(){
+this.products$.pipe(
+  (map((products:any)=>{
+    return products.filter((product:any)=>
+    product.active == true &&
+    product.stock>0 &&
+    product.name.toLowerCase().includes(this.searchText.toLowerCase())
+    )
+  }))
+).subscribe((filteredProducts)=>{
+  this.filteredProducts=filteredProducts
+})
+  }
+
+  selectProduct(productId:any,value:string){
+    const product=this.filteredProducts.find((product)=>product.id === productId);
+    const productIndex=this.selectedQuick.length -1;
+    const productFormGroup=this.selectedQuick.at(productIndex) as FormGroup;
+    productFormGroup.controls['id'].setValue(product.id);
+    this.searchText=value;
+    this.filteredProducts=[];
+  }
+    public confirmQuick() {
+    
+    console.log(this.quickForm.value);
+    this.http.post(`${this.quickApi}`, this.quickForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.toastr.success('New sale added successfully');
+      },
+      error: (error) => {
+        this.toastr.error('Product Quantity is Out of Range');
+      },
+      complete: () => {
+        console.log('Is good');
+      },
+    });
+    this.quickForm.reset();
+    this.searchText='';
+  
+  }
 }
