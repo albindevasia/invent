74e471103ac6898ef6e78ba34eb0a711625287ec
albindevasia <114718268+albindevasia@users.noreply.github.com>
2023-04-13 17:02:27 +0530

    tot

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -13,7 +13,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss'],
+
 })
 export class PaginationComponent implements OnChanges  {
 
